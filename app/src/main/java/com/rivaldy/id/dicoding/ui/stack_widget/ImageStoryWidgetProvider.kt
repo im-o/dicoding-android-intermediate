14e7a9dca046185d9fd6ae62cc4244aa9ae365d9
rivaldy <frizgt@gmail.com>
2022-10-20 08:51:14 +0800

    :label: feat: add sample stack widget

---
+++ app/src/main/java/com/rivaldy/id/dicoding/ui/stack_widget/ImageStoryWidgetProvider.kt
@@ -0,0 +1,56 @@
+package com.rivaldy.id.dicoding.ui.stack_widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.widget.RemoteViews
+import com.rivaldy.id.core.utils.UtilFunctions.logE
+import com.rivaldy.id.dicoding.R
+
+
+/** Created by github.com/im-o on 10/19/2022. */
+
+class ImageStoryWidgetProvider : AppWidgetProvider() {
+    override fun onReceive(context: Context?, intent: Intent) {
+        if (intent.action != null) {
+            if (intent.action == TOAST_ACTION) {
+                val viewIndex = intent.getIntExtra(EXTRA_ITEM, 0)
+                logE("onReceive You touch $viewIndex")
+            }
+        }
+        super.onReceive(context, intent)
+    }
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    companion object {
+        private const val TOAST_ACTION = "com.rivaldy.id.dicoding.TOAST_ACTION"
+        const val EXTRA_ITEM = "com.rivaldy.id.dicoding.EXTRA_ITEM"
+        fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+            val intent = Intent(context, StackWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME))
+            val views = RemoteViews(context.packageName, R.layout.image_story_widget)
+            views.setRemoteAdapter(R.id.widgetStackView, intent)
+            views.setEmptyView(R.id.widgetStackView, R.id.widgetNoDataTV)
+            val toastIntent = Intent(context, ImageStoryWidgetProvider::class.java)
+            toastIntent.action = TOAST_ACTION
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME))
+            val toastPendingIntent = PendingIntent.getBroadcast(
+                context, 0, toastIntent, if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE else 0
+            )
+            views.setPendingIntentTemplate(R.id.widgetStackView, toastPendingIntent)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
---
+++ app/src/main/java/com/rivaldy/id/dicoding/ui/stack_widget/StackRemoteViewsFactory.kt
@@ -0,0 +1,50 @@
+package com.rivaldy.id.dicoding.ui.stack_widget
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+import androidx.core.os.bundleOf
+import com.rivaldy.id.dicoding.R
+
+/** Created by github.com/im-o on 10/19/2022. */
+
+class StackRemoteViewsFactory(private val mContext: Context) : RemoteViewsService.RemoteViewsFactory {
+
+    private val mWidgetItems = ArrayList<Bitmap>()
+
+    override fun onCreate() {}
+
+    override fun onDataSetChanged() {
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.star_wars_logo))
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.starwars))
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.star_wars_logo))
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.starwars))
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.star_wars_logo))
+        mWidgetItems.add(BitmapFactory.decodeResource(mContext.resources, R.drawable.starwars))
+    }
+
+    override fun onDestroy() {}
+
+    override fun getCount(): Int = mWidgetItems.size
+
+    override fun getViewAt(position: Int): RemoteViews {
+        val rv = RemoteViews(mContext.packageName, R.layout.row_item_widget)
+        rv.setImageViewBitmap(R.id.imageStoryIV, mWidgetItems[position])
+        val extras = bundleOf(ImageStoryWidgetProvider.EXTRA_ITEM to position)
+        val fillInIntent = Intent()
+        fillInIntent.putExtras(extras)
+        rv.setOnClickFillInIntent(R.id.imageStoryIV, fillInIntent)
+        return rv
+    }
+
+    override fun getLoadingView(): RemoteViews? = null
+
+    override fun getViewTypeCount(): Int = 1
+
+    override fun getItemId(p0: Int): Long = 0
+
+    override fun hasStableIds(): Boolean = false
+}
