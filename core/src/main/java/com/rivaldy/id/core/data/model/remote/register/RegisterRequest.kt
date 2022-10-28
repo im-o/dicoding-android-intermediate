264f3d77a73eb5b2c4647f0e1709b545dae6def7
rivaldy <frizgt@gmail.com>
2022-10-28 14:53:23 +0800

    :bug: fixed register request

--- core/src/main/java/com/rivaldy/id/core/data/model/remote/register/RegisterRequest.kt
+++ core/src/main/java/com/rivaldy/id/core/data/model/remote/register/RegisterRequest.kt
@@ -4,10 +4,10 @@ package com.rivaldy.id.core.data.model.remote.register
 import com.google.gson.annotations.SerializedName
 
 data class RegisterRequest(
-    @SerializedName("email")
-    val email: String? = null,
     @SerializedName("name")
     val name: String? = null,
+    @SerializedName("email")
+    val email: String? = null,
     @SerializedName("password")
     val password: String? = null
 )
