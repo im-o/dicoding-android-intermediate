7e5033b7128af6590fe38b3834b2f75f39ad2d89
rivaldy <frizgt@gmail.com>
2022-10-30 16:20:06 +0800

    :hammer: refactor custom view function addTextChangedListener

--- commons/src/main/java/com/rivaldy/id/commons/widget/input/RegularInputEditText.kt
+++ commons/src/main/java/com/rivaldy/id/commons/widget/input/RegularInputEditText.kt
@@ -3,13 +3,12 @@ package com.rivaldy.id.commons.widget.input
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import com.rivaldy.id.commons.R
 
 
@@ -77,15 +76,8 @@ class RegularInputEditText : AppCompatEditText, View.OnTouchListener {
     private fun init() {
         clearButtonIcon = ContextCompat.getDrawable(context, R.drawable.ic_baseline_clear_24) as Drawable
         setOnTouchListener(this)
-
-        addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                if (s.toString().isNotEmpty()) showClearButton() else hideClearButton()
-            }
-
-            override fun afterTextChanged(s: Editable) {}
+        addTextChangedListener(onTextChanged = { p0, _, _, _ ->
+            if (p0.toString().isNotEmpty()) showClearButton() else hideClearButton()
         })
     }
 
