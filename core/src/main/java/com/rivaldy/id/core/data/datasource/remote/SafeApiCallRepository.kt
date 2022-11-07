4c3dc45906e6f3114517f6ab32df7f5b05a9365c
rivaldy <frizgt@gmail.com>
2022-11-07 21:14:54 +0800

    :white_check_mark: update unit testing for RestApiRepositoryImplTest.kt

--- core/src/main/java/com/rivaldy/id/core/data/datasource/remote/SafeApiCallRepository.kt
+++ core/src/main/java/com/rivaldy/id/core/data/datasource/remote/SafeApiCallRepository.kt
@@ -4,7 +4,6 @@ import com.rivaldy.id.core.data.network.DataResource
 import com.rivaldy.id.core.utils.UtilConstants.NO_INTERNET
 import com.rivaldy.id.core.utils.UtilConstants.OTHER_ERROR
 import com.rivaldy.id.core.utils.UtilFunctions.logE
-import com.rivaldy.id.core.utils.wrapEspressoIdlingResource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
@@ -14,20 +13,18 @@ import retrofit2.HttpException
 abstract class SafeApiCallRepository {
     suspend fun <T> safeApiCall(apiCall: suspend () -> T): DataResource<T> {
         return withContext(Dispatchers.IO) {
-            wrapEspressoIdlingResource {
-                try {
-                    DataResource.Success(apiCall.invoke())
-                } catch (throwable: Throwable) {
-                    logE(throwable.message.toString())
-                    when (throwable) {
-                        is HttpException -> {
-                            DataResource.Failure(false, throwable.code(), throwable.response()?.errorBody(), throwable.message)
-                        }
-                        else -> {
-                            if (throwable.message == NO_INTERNET) {
-                                DataResource.Failure(true, null, null, throwable.message)
-                            } else DataResource.Failure(true, OTHER_ERROR, null, throwable.message) // CHANGE THIS TO FALSE
-                        }
+            try {
+                DataResource.Success(apiCall.invoke())
+            } catch (throwable: Throwable) {
+                logE(throwable.message.toString())
+                when (throwable) {
+                    is HttpException -> {
+                        DataResource.Failure(false, throwable.code(), throwable.response()?.errorBody(), throwable.message)
+                    }
+                    else -> {
+                        if (throwable.message == NO_INTERNET) {
+                            DataResource.Failure(true, null, null, throwable.message)
+                        } else DataResource.Failure(true, OTHER_ERROR, null, throwable.message) // CHANGE THIS TO FALSE
                     }
                 }
             }
--- core/src/test/java/com/rivaldy/id/core/data/datasource/remote/rest/RestApiRepositoryImplTest.kt
+++ core/src/test/java/com/rivaldy/id/core/data/datasource/remote/rest/RestApiRepositoryImplTest.kt
@@ -2,6 +2,9 @@ package com.rivaldy.id.core.data.datasource.remote.rest
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.rivaldy.id.core.data.model.dummy.DummyData
+import com.rivaldy.id.core.data.model.remote.DefaultResponse
+import com.rivaldy.id.core.data.model.remote.login.LoginResponse
+import com.rivaldy.id.core.data.model.remote.register.RegisterResponse
 import com.rivaldy.id.core.data.network.DataResource
 import com.rivaldy.id.core.util.MainDispatcherRule
 import com.rivaldy.id.core.utils.UtilConstants.DEFAULT_LIMIT_PAGE
@@ -9,11 +12,12 @@ import com.rivaldy.id.core.utils.UtilConstants.ONLY_LOCATION_TYPE
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
 
 /** Created by github.com/im-o on 11/1/2022.  */
@@ -28,35 +32,40 @@ class RestApiRepositoryImplTest {
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
 
-    @Mock
+    private var apiService = mock(ApiService::class.java)
+
     private lateinit var repository: RestApiRepositoryImpl
+
+    @Before
+    fun setup() {
+        repository = RestApiRepositoryImpl(apiService)
+    }
 
     @Test
     fun `when user register, then return Success`() = runTest {
         val request = DummyData.dummyRegisterRequest()
         val expectedResponse = DummyData.dummyRegisterSuccess()
 
-        Mockito.`when`(repository.registerUserApiCall(request)).thenReturn(expectedResponse)
+        Mockito.`when`(apiService.registerUser(request.name ?: "", request.email ?: "", request.password ?: "")).thenReturn(expectedResponse.value)
         repository.registerUserApiCall(request).let {
             assertNotNull(it)
             assertTrue(it is DataResource.Success)
             assertEquals(expectedResponse, it)
         }
-        Mockito.verify(repository).registerUserApiCall(request)
+        Mockito.verify(apiService).registerUser(request.name ?: "", request.email ?: "", request.password ?: "")
     }
 
     @Test
     fun `when user register, then return Failure`() = runTest {
         val request = DummyData.dummyRegisterRequest()
-        val expectedResponse = DummyData.dummyRegisterFailure()
+        val expectedResponse = RegisterResponse(true, DummyData.dummyRegisterFailure().otherMessage)
 
-        Mockito.`when`(repository.registerUserApiCall(request)).thenReturn(expectedResponse)
+        Mockito.`when`(apiService.registerUser(request.name ?: "", request.email ?: "", request.password ?: "")).thenReturn(expectedResponse)
         repository.registerUserApiCall(request).let {
             assertNotNull(it)
-            assertTrue(it is DataResource.Failure)
-            assertEquals(expectedResponse, it)
+            assertEquals(expectedResponse, (it as DataResource.Success).value)
         }
-        Mockito.verify(repository).registerUserApiCall(request)
+        Mockito.verify(apiService).registerUser(request.name ?: "", request.email ?: "", request.password ?: "")
     }
 
     @Test
@@ -64,55 +73,54 @@ class RestApiRepositoryImplTest {
         val request = DummyData.dummyLoginRequest()
         val expectedResponse = DummyData.dummyLoginSuccess()
 
-        Mockito.`when`(repository.loginUserApiCall(request)).thenReturn(expectedResponse)
+        Mockito.`when`(apiService.loginUser(request.email ?: "", request.password ?: "")).thenReturn(expectedResponse.value)
         repository.loginUserApiCall(request).let {
             assertNotNull(it)
             assertTrue(it is DataResource.Success)
             assertEquals(expectedResponse, it)
         }
-        Mockito.verify(repository).loginUserApiCall(request)
+        Mockito.verify(apiService).loginUser(request.email ?: "", request.password ?: "")
     }
 
     @Test
     fun `when user login, then return Failure`() = runTest {
         val request = DummyData.dummyLoginRequest()
-        val expectedResponse = DummyData.dummyLoginFailure()
+        val expectedResponse = LoginResponse(true, null, DummyData.dummyLoginFailure().otherMessage)
 
-        Mockito.`when`(repository.loginUserApiCall(request)).thenReturn(expectedResponse)
+        Mockito.`when`(apiService.loginUser(request.email ?: "", request.password ?: "")).thenReturn(expectedResponse)
         repository.loginUserApiCall(request).let {
             assertNotNull(it)
-            assertTrue(it is DataResource.Failure)
-            assertEquals(expectedResponse, it)
+            assertEquals(expectedResponse, (it as DataResource.Success).value)
         }
-        Mockito.verify(repository).loginUserApiCall(request)
+        Mockito.verify(apiService).loginUser(request.email ?: "", request.password ?: "")
     }
 
     @Test
     fun `when user Load Story, then return Story List`() = runTest {
         val expectedResponse = DataResource.Success(DummyData.generateDummyUserStoryResponse())
 
-        Mockito.`when`(repository.getStoriesApiCall(size = DEFAULT_LIMIT_PAGE, locationType = ONLY_LOCATION_TYPE, page = null)).thenReturn(expectedResponse)
+        Mockito.`when`(apiService.getStories(size = DEFAULT_LIMIT_PAGE, location = ONLY_LOCATION_TYPE, page = null)).thenReturn(expectedResponse.value)
         repository.getStoriesApiCall(size = DEFAULT_LIMIT_PAGE, locationType = ONLY_LOCATION_TYPE, page = null).let {
             assertNotNull(it)
             assertTrue(it is DataResource.Success)
             assertTrue((it as DataResource.Success).value.listStory?.isNotEmpty() == true)
             assertEquals(expectedResponse, it)
         }
-        Mockito.verify(repository).getStoriesApiCall(size = DEFAULT_LIMIT_PAGE, locationType = ONLY_LOCATION_TYPE, page = null)
+        Mockito.verify(apiService).getStories(size = DEFAULT_LIMIT_PAGE, location = ONLY_LOCATION_TYPE, page = null)
     }
 
     @Test
     fun `when user Load Story, then return Empty List`() = runTest {
         val expectedResponse = DataResource.Success(DummyData.generateDummyUserStoryEmpty())
 
-        Mockito.`when`(repository.getStoriesApiCall(size = DEFAULT_LIMIT_PAGE, locationType = ONLY_LOCATION_TYPE, page = null)).thenReturn(expectedResponse)
+        Mockito.`when`(apiService.getStories(size = DEFAULT_LIMIT_PAGE, location = ONLY_LOCATION_TYPE, page = null)).thenReturn(expectedResponse.value)
         repository.getStoriesApiCall(size = DEFAULT_LIMIT_PAGE, locationType = ONLY_LOCATION_TYPE, page = null).let {
             assertNotNull(it)
             assertTrue(it is DataResource.Success)
             assertTrue((it as DataResource.Success).value.listStory?.isEmpty() == true)
             assertEquals(expectedResponse, it)
         }
-        Mockito.verify(repository).getStoriesApiCall(size = DEFAULT_LIMIT_PAGE, locationType = ONLY_LOCATION_TYPE, page = null)
+        Mockito.verify(apiService).getStories(size = DEFAULT_LIMIT_PAGE, location = ONLY_LOCATION_TYPE, page = null)
     }
 
     @Test
@@ -121,27 +129,26 @@ class RestApiRepositoryImplTest {
         val imageMultipart = DummyData.dummyImageMultipart
         val expectedResponse = DummyData.dummyAddStorySuccess()
 
-        Mockito.`when`(repository.addStoryApiCall(descriptionBody, imageMultipart, null, null)).thenReturn(expectedResponse)
+        Mockito.`when`(apiService.addStory(descriptionBody, imageMultipart, null, null)).thenReturn(expectedResponse.value)
         repository.addStoryApiCall(descriptionBody, imageMultipart, null, null).let {
             assertNotNull(it)
             assertTrue(it is DataResource.Success)
             assertEquals(expectedResponse, it)
         }
-        Mockito.verify(repository).addStoryApiCall(descriptionBody, imageMultipart, null, null)
+        Mockito.verify(apiService).addStory(descriptionBody, imageMultipart, null, null)
     }
 
     @Test
     fun `when user add story, then return Failure`() = runTest {
         val descriptionBody = DummyData.dummyDescriptionBody
         val imageMultipart = DummyData.dummyImageMultipart
-        val expectedResponse = DummyData.dummyAddStoryFailure()
+        val expectedResponse = DefaultResponse(false, DummyData.dummyAddStoryFailure().otherMessage)
 
-        Mockito.`when`(repository.addStoryApiCall(descriptionBody, imageMultipart, null, null)).thenReturn(expectedResponse)
+        Mockito.`when`(apiService.addStory(descriptionBody, imageMultipart, null, null)).thenReturn(expectedResponse)
         repository.addStoryApiCall(descriptionBody, imageMultipart, null, null).let {
             assertNotNull(it)
-            assertTrue(it is DataResource.Failure)
-            assertEquals(expectedResponse, it)
+            assertEquals(expectedResponse, (it as DataResource.Success).value)
         }
-        Mockito.verify(repository).addStoryApiCall(descriptionBody, imageMultipart, null, null)
+        Mockito.verify(apiService).addStory(descriptionBody, imageMultipart, null, null)
     }
 }
